0c47def0bd2e7388d78442344c01c194bdea771c
thaone31 <99576488+thaone31@users.noreply.github.com>
2024-01-06 23:01:03 +0700

    Update and rename createvncore.py to download.py

--- download.py
+++ download.py
@@ -1,6 +1,12 @@
 import os
 import urllib.request
+import subprocess
 
+# Sử dụng lệnh wget để tải về file
+subprocess.run(['wget', 'https://public.vinai.io/PhoBERT_base_transformers.tar.gz'])
+
+# Sử dụng lệnh tar để giải nén file
+subprocess.run(['tar', '-xzvf', 'PhoBERT_base_transformers.tar.gz'])
 # Tạo thư mục chứa các tệp cần thiết
 os.makedirs('vncorenlp/models/wordsegmenter', exist_ok=True)
 
